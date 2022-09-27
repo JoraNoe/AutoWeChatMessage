7a272419924fe00e63b9714d7ab58b43f31361bb
JoreNoe <57209261+JoraNoe@users.noreply.github.com>
2022-09-27 18:04:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 import Gaode
 import WeChat
 
+# 获取access_token
+def getAccessToken():
+    r1 = requests.get(
+        url="https://api.weixin.qq.com/cgi-bin/token",
+        params={
+            "grant_type": "client_credential",
+            "appid": appID,
+            "secret": appsecret
+        }
+    )
+    access_token = r1.json()['access_token']
+    # print(access_token)
+    return access_token
+
+
+
 # 微信id列表 给谁发加谁
 wx_id = ["oB63O6s0JNpJPxOtiUDWnGMlMwpI","oB63O6mW5gDQKJyFk2FLXSBdph7E","oB63O6lPUOtE7JaaMfUWUd-9RR7Y"]
 # 模板消息id
