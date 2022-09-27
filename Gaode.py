70994000c9db9beb2c25ae5b49d3e746a19e2c74
JoreNoe <57209261+JoraNoe@users.noreply.github.com>
2022-09-27 16:56:46 +0800

    Update Gaode.py

--- Gaode.py
+++ Gaode.py
@@ -3,11 +3,11 @@ import requests
 # 高德天气查询平台 https://lbs.amap.com/api/webservice/guide/api/weatherinfo
 # 选择Web服务获取key 教程：https://blog.csdn.net/gz2580/article/details/111088190
 
-key = 'xxxxxxxxxxxxxxxxxxxxxxx'
+key = '0192d5d16230c2e6b6a3e79529fc34a6'
 # adcode 城市编码表下载：https://lbs.amap.com/api/webservice/download
 # 江夏区 420115
 # 大同城区 140213
-city_code = 'xxxxxx'
+city_code = '130400'
 
 weather_url = f'https://restapi.amap.com/v3/weather/weatherInfo?key={key}&city={city_code}'
 
