7acfe20b736a006f4c5aac73fc7a402abd94e471
JoreNoe <57209261+JoraNoe@users.noreply.github.com>
2022-09-27 17:31:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import WeChat
 # 微信id列表 给谁发加谁
 wx_id = ["oB63O6s0JNpJPxOtiUDWnGMlMwpI","oB63O6mW5gDQKJyFk2FLXSBdph7E"]
 # 模板消息id
-template_id = 'iPqP0v0KvpQAP_1X_iv79EYpRyD7Q8TyznMV4FGW-no	'
+template_id = 'iPqP0v0KvpQAP_1X_iv79EYpRyD7Q8TyznMV4FGW-no'
 
 # 获取天气数据
 weatherData = Gaode.getWeather()
