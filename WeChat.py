7a6f0ebe8bdaf76ef74c3a61050d094c4246388f
JoreNoe <57209261+JoraNoe@users.noreply.github.com>
2022-10-12 14:52:01 +0800

    Update WeChat.py

--- WeChat.py
+++ WeChat.py
@@ -23,7 +23,7 @@ def getAccessToken():
     # print(access_token)
     return access_token
 
-#获取情话接口
+# 获取情话接口
 def getLoveLanage():
         loveLanageResult = requests.get("https://api.1314.cool/words/api.php")
         return loveLanageResult
