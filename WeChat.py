8124b9eaea10d9f7922f1196240bec6cecb5f41e
JoreNoe <57209261+JoraNoe@users.noreply.github.com>
2023-03-01 14:15:56 +0800

    Update WeChat.py

--- WeChat.py
+++ WeChat.py
@@ -47,7 +47,10 @@ def sendMessage(weatherData, wx_id, template_id):
     togetherDay = datetime.date(2022, 11, 26)
     # 她的下一个生日
     herBirthday = datetime.date(2023, 4, 4)
-
+    
+    #老婆考试日期
+    ExeamDay = daatetime.date(2023,4,1);
+    
     today = datetime.date(int(year), int(month), int(day))
     week_list = ["星期一", "星期二", "星期三", "星期四", "星期五", "星期六", "星期日"]
     body = {
@@ -95,7 +98,7 @@ def sendMessage(weatherData, wx_id, template_id):
                 'color': get_color()
             },
             'speack': {  # 情话
-                'value': getLoveLanage(),
+                'value': getLoveLanage() + ' -老婆加油，老婆最棒，老公支持你，老婆使劲，老婆奥利给- 好好学习,距离你考试剩余：'+int(re.search('(?P<days>.*?) days', str(ExeamDay.__sub__(today))).group('days'))+'天.'
                 'color': get_color()
             }
         }
